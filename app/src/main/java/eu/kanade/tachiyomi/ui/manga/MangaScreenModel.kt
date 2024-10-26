633a1892b310d9b86fe05d64d40677bcea37d413
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2024-10-27 02:16:39 +1300

    Allow completely disabling "Update tracker" snackbar on mark as read (#1374)
    Also fixes #1369
    
    (cherry picked from commit fc2f339ea1cdc43c0041b2fed497dcfda853b85e)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
@@ -29,6 +29,7 @@ import eu.kanade.domain.manga.model.toSManga
 import eu.kanade.domain.source.service.SourcePreferences
 import eu.kanade.domain.track.interactor.AddTracks
 import eu.kanade.domain.track.interactor.TrackChapter
+import eu.kanade.domain.track.model.AutoTrackState
 import eu.kanade.domain.track.model.toDomainTrack
 import eu.kanade.domain.track.service.TrackPreferences
 import eu.kanade.domain.ui.UiPreferences
@@ -211,6 +212,7 @@ class MangaScreenModel(
 
     val chapterSwipeStartAction = libraryPreferences.swipeToEndAction().get()
     val chapterSwipeEndAction = libraryPreferences.swipeToStartAction().get()
+    var autoTrackState = trackPreferences.autoUpdateTrackOnMarkRead().get()
 
     private val skipFiltered by readerPreferences.skipFiltered().asState(screenModelScope)
 
@@ -1267,15 +1269,19 @@ class MangaScreenModel(
                 chapters = chapters.toTypedArray(),
             )
 
-            if (!read) return@launchIO
+            if (
+                successState?.hasLoggedInTrackers == false ||
+                !read || autoTrackState == AutoTrackState.NEVER
+            ) {
+                return@launchIO
+            }
 
             val tracks = getTracks.await(mangaId)
             val maxChapterNumber = chapters.maxOf { it.chapterNumber }
             val shouldPromptTrackingUpdate = tracks.any { track -> maxChapterNumber > track.lastChapterRead }
 
             if (!shouldPromptTrackingUpdate) return@launchIO
-
-            if (trackPreferences.autoUpdateTrackOnMarkRead().get()) {
+            if (autoTrackState == AutoTrackState.ALWAYS) {
                 trackChapter.await(context, mangaId, maxChapterNumber)
                 withUIContext {
                     context.toast(context.stringResource(MR.strings.trackers_updated_summary, maxChapterNumber.toInt()))
